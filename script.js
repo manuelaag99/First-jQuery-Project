5e65b24fdd69a18df4a512ab272fe50b9eb33674
Manuel Alanis <manuelaag99@gmail.com>
2022-12-23 23:35:04 -0600

    20221223 Last Commit

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 
 var extraText = "<button>Click here!</button>";
 
+//this section assigns letter values to each box 
 $("#after").click(function() {
     var click = "a";
+    buttonsAction(click);
 })
 
 $("#before").click(function() {
     var click = "s";
+    buttonsAction(click);
 })
 
 $("#append").click(function() {
     var click = "d";
+    buttonsAction(click);
 })
 
 $("#prepend").click(function() {
     var click = "f";
+    buttonsAction(click);
 })
 
 $("#return").click(function() {
     var click = "r";
+    buttonsAction(click);
 })
 
 
-
-
+//this section 
 $(document).keypress(function() {
-    //must check the status of the "event" property 
     var press = event.key;
+    buttonsAction(press);
+})
 
-    switch (press) {
+function buttonsAction(input) {
+    switch (input) {
         case "a":
             $("h4").text("<h2>Sample text</h2>" + extraText); //"after"
             $("h2").html("<h2>Sample text</h2>" + extraText); //"after"            
@@ -48,11 +55,16 @@ $(document).keypress(function() {
             $("h4").text("<h2>" + extraText + "Sample text</h2>"); //"prepend"
             $("h2").html("<h2>" + extraText + "Sample text</h2>"); //"prepend"
             break;
+        
+        case "r":
+            $("h4").text("Sample text"); 
+            $("h2").html("Sample text"); 
+            break;
             
         default: 
-        console.log(press);
+        console.log(input);
             break;
     }
+}
 
-
-})
+function clickAction()
